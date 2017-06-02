27447e3070fc7b52493cd2738885a85f2e7f7bc4
sheshankyadav8 <sheshankyadav8@gmail.com>
2017-06-01 21:01:46 -0700

    standaloneapp source code

---
+++ src/main/java/com/ibm/services/WebApp.java
@@ -0,0 +1,13 @@
+package com.ibm.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class WebApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
